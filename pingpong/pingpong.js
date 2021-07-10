8ddbd8b2d21508624fb36f41cbf992ecd9cfa24b
Sohye Lee <61291530+sohye-lee@users.noreply.github.com>
2021-07-09 23:33:43 -0400

    paddle

--- pingpong/pingpong.js
+++ pingpong/pingpong.js
@@ -1,11 +1,13 @@
 var canvas, canvasContext;
 var ballX = 75;
 var ballY = 75
+var ballSize = 20;
 var ballSpeedX = 5;
 var ballSpeedY = 7;
 
 const paddle_width = 100;
 const paddle_thickness = 10;
+const paddle_from_edge = 50;
 var paddleX = 400;
 
 function updateMousePos(event) {
@@ -33,28 +35,51 @@ function updateAll() {
     moveAll();
     drawAll();
 }
+
+function ballReset() {
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
+
 function moveAll() {
     ballX += ballSpeedX;
     ballY += ballSpeedY;
 
-    if (ballX > canvas.width) {
+    if (ballX + ballSize > canvas.width) {
         ballSpeedX *= -1;
     }
-    if (ballX < 0) {
+    if (ballX - ballSize < 0) {
         ballSpeedX *= -1;
     }
-    if (ballY > canvas.height) {
+    if (ballY + ballSize > canvas.height) {
+        ballReset();
+    }
+    if (ballY - ballSize < 0) {
         ballSpeedY *= -1;
     }
-    if (ballY < 0) {
-        ballSpeedY *= -1;
+
+    var paddleTopEdgeY = canvas.height - paddle_from_edge;
+    var paddleBottomEdgeY = paddleTopEdgeY + paddle_thickness;
+    var paddleLeftEdgeX = paddleX;
+    var paddleRightEdgeX = paddleX + paddle_width;
+
+    if (ballY + ballSize  > paddleTopEdgeY && ballY + ballSize < paddleBottomEdgeY &&
+        ballX  > paddleLeftEdgeX && ballX < paddleRightEdgeX) {
+        
+            ballSpeedY *= -1;
+
+            var centerOfPaddleX = paddleX + paddle_width/2;
+            var ballDistFromPaddleCenter = ballX - centerOfPaddleX;
+            ballSpeedX = ballDistFromPaddleCenter * 0.35;
     }
+
+    
 }
 
 function drawAll() {
     colorRect(0,0, canvas.width, canvas.height, "black");
-    colorCircle(ballX, ballY, 20, 'white');
-    colorRect(paddleX, canvas.height - paddle_thickness, 
+    colorCircle(ballX, ballY, ballSize, 'white');
+    colorRect(paddleX, canvas.height - paddle_from_edge, 
         paddle_width, paddle_thickness, 'white');
 }
 
